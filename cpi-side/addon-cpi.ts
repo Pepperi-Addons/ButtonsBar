a284d9e650f12d164b3885d740e9d0d121b82d09
Avner Eisenberg <avner.e@pepperi.com>
2023-10-10 16:25:27 +0300

    fix cpi side bug

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -16,24 +16,28 @@ export const router = Router()
 // })
 
 router.post('/run_on_load_event', async (req, res) => {
-    let configuration = req?.body?.Configuration;
+    const configuration = req?.body?.Configuration;
+    let configurationRes = configuration;
     const state = req.body.State;
     // check if flow configured to on load --> run flow (instaed of onload event)
     if (configuration?.ButtonsBarConfig?.OnLoadFlow){
         const cpiService = new ButtonsBarCpiService();
         //CALL TO FLOWS AND SET CONFIGURATION
         const result: any = await cpiService.getOptionsFromFlow(configuration.ButtonsBarConfig.OnLoadFlow || [], state, req.context, configuration);
-        configuration = result?.configuration || configuration;
+        configurationRes = result?.configuration || configuration; 
     }
-    res.json({Configuration: configuration});
+    const mergeState = Object.assign(Object.assign({}, state), {configuration: configurationRes});
+    res.json({
+        State: mergeState,
+        Configuration: configurationRes,
+    });
 });
 
 router.post('/run_button_click_event', async (req, res) => {
-    let configurationRes;
-    let mergeState
     const state = req.body.State;
     const btnID = req.body.ButtonKey;
     const configuration = state?.configuration || req?.body?.Configuration;
+    let configurationRes = configuration;
     // check if flow configured to on load --> run flow (instaed of onload event)
     if (configuration?.Buttons[btnID]?.Flow){
         const cpiService = new ButtonsBarCpiService();
@@ -41,33 +45,22 @@ router.post('/run_button_click_event', async (req, res) => {
         const result: any = await cpiService.getOptionsFromFlow(configuration.Buttons[btnID].Flow || [], state, req.context, configuration);
         //Statechanges = _.differenceWith(_.toPairs(result.configuration), _.toPairs(configuration), _.isEqual);
         configurationRes = result?.configuration || configuration;
-        mergeState = Object.assign(Object.assign({}, state), {configuration: configurationRes});
     }
+    const  mergeState = Object.assign(Object.assign({}, state), {configuration: configurationRes});
     res.json({
         State: mergeState,
         Configuration: configurationRes,
     });
-    res.json({Configuration: configurationRes});
 });
 
 router.post('/on_block_state_change', async (req, res) => {
     const state = req.body.State || {};
     const changes = req.body.Changes || {};
-    const configuration = req.body.Configuration;
-    const cpiService = new ButtonsBarCpiService();
+    //const configuration = req.body.Configuration;
 
     const mergeState = {...state, ...changes};
-    //configuration.filters = await cpiService.PrepareFiltersData(configuration.filters || [], mergeState, req.context);
     res.json({
         State: mergeState,
         Configuration: changes,
     });
 });
-
-/**********************************  client events starts /**********************************/
-// pepperi.events.intercept(CLIENT_ACTION_ON_BUTTONS_BAR_CLICK as any, {}, async (data): Promise<any> => {
-//     const cpiService = new ButtonsBarCpiService();
-//     const res: any = await cpiService.getOptionsFromFlow(data.flow, data.parameters, data, data.parameters.configuration );
-//     return res;
-// });
-/***********************************  client events ends /***********************************/
